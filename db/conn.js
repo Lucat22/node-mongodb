3685e008e5116dd24f8350f54c072d9fc3e4197f
Lucat22 <lucalvins1@gmail.com>
2024-03-20 21:55:57 -0300

    Criando conexão com o banco

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function main(){
 
     try {
-    await mongoose.connect('mongodb+srv://lucasm:Pulh3ObV7iUijTPj@cluster0.jpof0et.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.jpof0et.mongodb.net/?retryWrites=true&w=majority`)
     console.log('Banco OK');
     
 } catch (error) {
